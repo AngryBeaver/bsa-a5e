4d63c7256aa5467a1bb17702aa0032c13d09375f
AngryBeaver <github@verplanmich.net>
2024-08-29 11:28:20 +0200

    add modular Tests

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ Hooks.on("beavers-system-interface.init", async function(){
 //update Actor will not update ui window in this system !
 Hooks.on("updateActor",(actor,flags,diff,id)=>{
     const appId = actor?._sheet?.appId
-    //special fix for beavers-crafting this is all evil !
+    //special fix for beavers-crafting this is all evil think about ways to remove it here!
     if(ui.windows[appId] && ui.windows[appId].beaversCraftingTabSheet){
         ui.windows[appId].beaversCraftingTabSheet.init();
     }
